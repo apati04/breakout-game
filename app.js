8fe3d57002a4428f2d4bd926e37deb369cfeb797
Andrew Patipaksiri <apax714@gmail.com>
2017-04-15 11:14:28 -0700

    adds blue rectangle no fill

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ ctx.arc(240, 160, 20, 0, Math.PI*2, false);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
+
+// border only rectangle
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+ctx.stroke();
+ctx.closePath();
